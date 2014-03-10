263934d50b4ab2d25a3bf4dced70e011a32ad0ba
Gregory Estrade <gregory.estrade@gmail.com>
2014-03-10 07:34:00 +0100

    firebee: PSC (UART) emulation.

--- include/hw/m68k/mcf.h
+++ include/hw/m68k/mcf.h
@@ -12,6 +12,16 @@ void mcf_uart_write(void *opaque, hwaddr addr,
 void *mcf_uart_init(qemu_irq irq, Chardev *chr);
 void mcf_uart_mm_init(hwaddr base, qemu_irq irq, Chardev *chr);
 
+/* mcf_psc.c */
+uint64_t mcf_psc_read(void *opaque, hwaddr addr,
+                      unsigned size);
+void mcf_psc_write(void *opaque, hwaddr addr,
+                   uint64_t val, unsigned size);
+void *mcf_psc_init(qemu_irq irq, CharDriverState *chr);
+void mcf_psc_mm_init(struct MemoryRegion *sysmem,
+                     hwaddr base,
+                     qemu_irq irq, CharDriverState *chr);
+
 /* mcf_intc.c */
 qemu_irq *mcf_intc_init(struct MemoryRegion *sysmem,
                         hwaddr base,
